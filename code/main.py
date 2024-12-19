1173cfe9ad03c09a9af651f74e934aa7a5e5eefe
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-12-19 08:41:39 +0300

    colision with static objects and the collision sprites class

--- code/main.py
+++ code/main.py
@@ -15,13 +15,14 @@ class Game:
 
         # groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
         # sprites
-        self.player = Player((WINDOW_WIDTH/2, WINDOW_HEIGHT/2) ,self.all_sprites)
+        self.player = Player((WINDOW_WIDTH/2, WINDOW_HEIGHT/2), self.collision_sprites,  self.all_sprites)
         for i in range(6):
             x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
             w, h = randint(60, 100), randint(60, 100)
-            CollisionSprite((x, y), (w, h), self.all_sprites)
+            CollisionSprite((x, y), (w, h), self.all_sprites, self.collision_sprites)
 
     def run(self):
         while self.running:
